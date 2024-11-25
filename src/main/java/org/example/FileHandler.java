7833a57ea451ad04b8bb644e0f4650cdbd1b20ff
kriptorbat <stasan.asanov@gmail.com>
2024-11-25 15:30:29 +0300

    fix getDuplicate, jsonParser, csvParser

--- src/main/java/org/example/FileHandler.java
+++ src/main/java/org/example/FileHandler.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
@@ -13,12 +14,22 @@ public class FileHandler {
     public void fileRecognizer(String filePath) throws IOException {
         List<EntityObject> entities = new ArrayList<EntityObject>();
         if (filePath.endsWith(".json")) {
-            String jsonStr = Files.readString(Path.of(filePath));
-            entities = jsonParser(jsonStr);
+            BufferedReader reader = Files.newBufferedReader(Path.of(filePath));
+            String line;
+            StringBuilder jsonStr = new StringBuilder();
+            //String jsonStr = Files.readString(Path.of(filePath));
+            while ((line = reader.readLine()) != null) {
+                jsonStr.append(line);
+            }
+            entities = jsonParser(jsonStr.toString());
             outputStatistic(entities);
-        } else if(filePath.endsWith(".csv")){
-            List<String> strLines = Files.readAllLines(Path.of(filePath));
-            entities = csvParser(strLines);
+        } else if (filePath.endsWith(".csv")) {
+            BufferedReader reader = Files.newBufferedReader(Path.of(filePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.equals("group,type,number,weight")) continue;
+                entities.add(csvParser(line));
+            }
             outputStatistic(entities);
         } else {
             System.out.println("Файл по пути " + filePath + " с нужным расширением не был найден");
@@ -29,7 +40,6 @@ public class FileHandler {
         List<EntityObject> entities = new ArrayList<>();
 
         jsonStr = jsonStr.replaceAll("\\[", "").replaceAll("]", "");
-
         String[] entityFields = jsonStr.split("},\\{");
 
         for (String field : entityFields) {
@@ -43,35 +53,29 @@ public class FileHandler {
             entity.setWeight(Long.parseLong(fields[3].split(":")[1]));
             entities.add(entity);
         }
-
         return entities;
     }
-    private List<EntityObject> csvParser(List<String> strLines) {
-        List<EntityObject> entities = new ArrayList<>();
-        for(String strLine : strLines){
-            if(strLine.equals("group,type,number,weight")) continue;
 
-            String[] fields = strLine.split(",");
-            EntityObject entity = new EntityObject();
-            entity.setGroup(fields[0]);
-            entity.setType(fields[1]);
-            entity.setNumber(Long.parseLong(fields[2]));
-            entity.setWeight(Long.parseLong(fields[3]));
-            entities.add(entity);
-        }
-        return entities;
+    private EntityObject csvParser(String line) {
+        String[] fields = line.split(",");
+        EntityObject entity = new EntityObject();
+        entity.setGroup(fields[0]);
+        entity.setType(fields[1]);
+        entity.setNumber(Long.parseLong(fields[2]));
+        entity.setWeight(Long.parseLong(fields[3]));
+        return entity;
     }
 
     private void outputStatistic(List<EntityObject> entities) {
         System.out.println("Дубликаты:");
-        for(String entityStr : getDuplicate(entities)) System.out.println(entityStr);
+        for (String entityStr : getDuplicate(entities)) System.out.println(entityStr);
         System.out.println();
 
-        System.out.println("Сумарный вес по гуппам:" );
+        System.out.println("Сумарный вес по гуппам:");
         for (Map.Entry<String, String> pair : getTotalWeight(entities).entrySet()) {
             String key = pair.getKey();
             String vol = pair.getValue();
-            System.out.println("Гуппа: " + key +" Суммарный вес = "+ vol);
+            System.out.println("Гуппа: " + key + " Суммарный вес = " + vol);
         }
         System.out.println();
 
@@ -82,61 +86,55 @@ public class FileHandler {
     }
 
     private List<String> getDuplicate(List<EntityObject> entities) {
+        Map<String,Integer> counts = new HashMap<>();
         List<String> duplicates = new ArrayList<>();
-        List<String> uniqueEntries = new ArrayList<>();
-        List<Integer> counts = new ArrayList<>();
-
         for (EntityObject entity : entities) {
             String entry = "Group = " + entity.getGroup() + " Type = " + entity.getType();
-
-            int index = uniqueEntries.indexOf(entry);
-            if (index != -1) {
-                counts.set(index, counts.get(index) + 1);
-            } else {
-                uniqueEntries.add(entry);
-                counts.add(1);
-            }
+            counts.put(entry, counts.getOrDefault(entry, 0) + 1);
         }
-        for (int i = 0; i < uniqueEntries.size(); i++) {
-            if (counts.get(i) > 1) {
-                duplicates.add(uniqueEntries.get(i) + " (количество повторений дубликатов: " + counts.get(i) + ")");
+
+        for (Map.Entry<String,Integer> entry : counts.entrySet()) {
+            if (entry.getValue() > 1) {
+                duplicates.add(entry.getKey() + " (количество повторений дубликатов: " + entry.getValue() + ")");
             }
         }
         return duplicates;
     }
-    private Map<String,String> getTotalWeight(List<EntityObject> entities){
-        Map<String,String> listSumTotalWeight = new HashMap<>();
-        for(EntityObject entity: entities){
-            if(listSumTotalWeight.get(entity.getGroup()) == null){
+
+    private Map<String, String> getTotalWeight(List<EntityObject> entities) {
+        Map<String, String> listSumTotalWeight = new HashMap<>();
+        for (EntityObject entity : entities) {
+            if (listSumTotalWeight.get(entity.getGroup()) == null) {
                 BigInteger sum = new BigInteger("0");
-                for(EntityObject entity2: entities){
-                    if(entity2.getGroup().equals(entity.getGroup())){
+                for (EntityObject entity2 : entities) {
+                    if (entity2.getGroup().equals(entity.getGroup())) {
                         BigInteger entity2weight = new BigInteger(Long.toString(entity2.getWeight()));
                         sum = sum.add(entity2weight);
                     }
                 }
-                listSumTotalWeight.put(entity.getGroup(),sum.toString());
+                listSumTotalWeight.put(entity.getGroup(), sum.toString());
             }
         }
         return listSumTotalWeight;
     }
-    private String getMaxWeight(List<EntityObject> entities){
+
+    private String getMaxWeight(List<EntityObject> entities) {
         long max = 0;
-        for(EntityObject entity: entities){
-            if(max < entity.getWeight()){
+        for (EntityObject entity : entities) {
+            if (max < entity.getWeight()) {
                 max = entity.getWeight();
             }
         }
         return Long.toString(max);
     }
-    private String getMinWeight(List<EntityObject> entities){
-        long min = 0L;
-        for(EntityObject entity: entities){
-            if(min > entity.getWeight() || min == 0){
+
+    private String getMinWeight(List<EntityObject> entities) {
+        long min = 0;
+        for (EntityObject entity : entities) {
+            if (min > entity.getWeight() || min == 0) {
                 min = entity.getWeight();
             }
         }
         return Long.toString(min);
     }
-
 }
